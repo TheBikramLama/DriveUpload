362cc75c1a726b313b4b6322d00a7089598d0ef6
Bikram Lama <thebikramlama1123@gmail.com>
2020-11-01 23:56:27 +0545

    Added arguments system

--- upload.sh
+++ upload.sh
@@ -2,44 +2,44 @@
 
 # Command Checker function
 checkCommand() {
-    local commands=(gdrive aria2c)
-    for command in "${commands[@]}"
-    do
-        if hash $command 2>/dev/null; then
-            continue
-        else
-            clear
-            echo "${command} is not installed."
-            echo "Please wait while the system installs required packages."
-            echo ""
+	local commands=(gdrive aria2c)
+	for command in "${commands[@]}"
+	do
+		if hash $command 2>/dev/null; then
+			continue
+		else
+			clear
+			echo "${command} is not installed."
+			echo "Please wait while the system installs required packages."
+			echo ""
 
 			# Re-initialize packages
 			./init.sh
 			continue
-        fi
-    done
+		fi
+	done
 }
 
 # Gdrive Login Checker
 checkGdrive() {
-    clear
-    gdrive about
-    clear
+	clear
+	gdrive about
+	clear
 }
 
 # Downloader Function
 startDownload() {
-    clear
-    aria2c -o "$name" "$url"
+	clear
+	aria2c -o "$name" "$url"
 }
 
 # Uploader Function
 startUpload() {
-    clear
-    # If folderId is empty upload to root directory
+	clear
+	# If folderId is empty upload to root directory
 	if [ -z "${folderid}" ]; then
 		gdrive upload "$name" --delete
-	else 
+	else
 		gdrive upload --parent "$folderid" "$name" --delete
 	fi
 }
@@ -49,11 +49,38 @@ checkCommand
 # Check if Gdrive is logged in
 checkGdrive
 
+# Check if arguments passed from outside
+while getopts ":u:n:f:" opt; do
+	case $opt in
+		u)
+			url=$OPTARG
+			;;
+		n)
+			name=$OPTARG
+			;;
+		f)
+			folderid=$OPTARG
+			;;
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			exit 1
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument." >&2
+			exit 1
+			;;
+	esac
+done
+
 # Download Type Selection Menu
 clear
-read -p "Download URL: " url
-read -p "Desired Name: " name
-read -p "Folder ID:(empty for root) " folderid
+if [ -z "${url}" ]; then
+	read -p "Download URL: " url
+fi
+if [ -z "${name}" ]; then
+	read -p "Desired Name: " name
+	read -p "Folder ID:(empty for root) " folderid
+fi
 
 # Start Download process
 startDownload
